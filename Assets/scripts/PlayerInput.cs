4d872ac9d56081a1e61f916159aa417fbb5ee4e3
Bilguun <soproatlol@gmail.com>
2020-12-17 18:54:59 +0800

    init

---
+++ Assets/scripts/PlayerInput.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using DG.Tweening;
+
+[DefaultExecutionOrder(-100)]
+
+
+public class PlayerInput : MonoBehaviour
+{
+     public float horizontal;
+     public bool jumpHeld;
+     public bool jumpPressed;
+     public bool crouchHeld;
+     public bool crouchPressed;
+
+    bool readyToClear;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        ClearInput();
+        ProcessInputs();
+        
+        horizontal = Mathf.Clamp(horizontal, -1f, 1f);
+        Debug.Log(horizontal.ToString());
+    }
+
+    void FixedUpdate(){
+        readyToClear = true;
+    }
+
+    void ClearInput(){
+        if(!readyToClear){
+            return;
+        }
+
+        horizontal = 0f;
+        
+        jumpPressed = false;
+        jumpHeld = false;
+        crouchPressed = false;
+        crouchHeld = false;
+        readyToClear = false;
+    }
+
+    void ProcessInputs()
+	{
+		//Accumulate horizontal axis input
+		horizontal		+= Input.GetAxis("Horizontal");
+        // horizontal		= Input.GetAxis("Horizontal");
+
+		//Accumulate button inputs
+		jumpPressed		= jumpPressed || Input.GetButtonDown("Jump");
+        Debug.Log(jumpPressed);
+		jumpHeld		= jumpHeld || Input.GetButton("Jump");
+
+		crouchPressed	= crouchPressed || Input.GetButtonDown("Crouch");
+		crouchHeld		= crouchHeld || Input.GetButton("Crouch");
+	}
+
+}
